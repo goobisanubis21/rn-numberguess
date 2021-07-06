75a16d4bd5aed6cc9c8144861611dcf39b534ad0
Anthony Parrino <anthony21parrino21@gmail.com>
2021-07-06 17:50:28 -0400

    update

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { Button, StyleSheet, Text, View, Alert } from 'react-native'
+import { StyleSheet, Text, View, Alert } from 'react-native'
 import NumberContainer from '../components/NumberContainer'
 import Card from '../components/Card'
 import MainButton from '../components/MainButton'
